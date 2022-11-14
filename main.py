990019bf00e9ba811b59e4f990d27a2374b625ec
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-11-14 11:53:34 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+ 
+"""print name and age! First define the variables
+with name and age. This is a multiline comments"""
+""" Beneath this multiple lines you will 
+see my code that sums up numbers.
+"""
+
+number = 1
+number = 2
+print(1+2)
+
+# printing name and age
+name = "Ozzie" #my name as variable
+age = 49
+print(name, age) #name and age printed out from variables
